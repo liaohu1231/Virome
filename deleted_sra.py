1d80a33a0e234b2fecd78a46d18ff484b14f2d07
liaohu1231 <59506038+liaohu1231@users.noreply.github.com>
2020-01-15 09:12:10 +0800

    Update deleted_sra.py

--- deleted_sra.py
+++ deleted_sra.py
@@ -6,8 +6,9 @@ path=os.path.dirname(os.path.abspath(__file__))
 files = os.listdir(path)
 file_out=[]
 dat_out=open("Hu_virome_fecal_total.fna","w")
-study="SRP049645,SRP002430,SRP098739"
-studies =study.split(",")
+study=open("except_sra.txt","r")
+studies =study.read()
+studies=studies.split("\n")
 
 for file in files:
     file_tail= file.split('.')[-1]
@@ -16,15 +17,17 @@ for file in files:
         dat=open(file,"r")
         dat_line=dat.read()
         dat_lines=dat_line.split(">")
-        for line in dat_lines:
-            name=line.split("|")[0]
-            print(name)
-            for sra in studies:
-                print(sra+" not")
-                if sra!=name: ###名字不匹配，写入file_out
+        for sra in studies:
+            #print(sra+"_not")
+            for line in dat_lines:
+                name=line.split("|")[0]
+                #print(name)
+                if name==sra:
+                    print(sra)
                     file_out.append(">"+str(line)+"\n")
-for lh in file_out:
-    dat_out.write(str(lh))
+                    
+for res in file_out:
+    dat_out.write(str(res))
 
 dat.close()
 dat_out.close()
