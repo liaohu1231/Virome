f57de410c877f53397562b9fed8036ec7d0d84b4
liaohu1231 <59506038+liaohu1231@users.noreply.github.com>
2020-01-16 17:48:48 +0800

    Add files via upload

---
+++ for_not_in_refseq_BV.py
@@ -0,0 +1,26 @@
+import os
+
+f1=open("evalue.txt","r")
+f1=f1.read()
+f1=f1.split("\n")
+name=[]
+sequence=[]
+
+for line in f1:
+    line_name=line.split("\t")[0]
+    if line_name not in name:
+        name.append(line_name)
+
+f2=open("Hu_final.contig.res_3000.fa","r")
+f3=open("refseq_bacteria_not_aln.fna","w")
+f2=f2.read()
+f2=f2.split(">")
+print(name)
+
+for seq in f2:
+    seq_id=seq.split("\n")[0]
+    if seq_id not in name:
+        sequence.append(">"+str(seq))
+
+for lh in sequence:
+    f3.write(lh)
