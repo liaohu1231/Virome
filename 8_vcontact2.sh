e0a4308eb113dc8e6dc4533fdec2000877225d27
liaohu1231 <59506038+liaohu1231@users.noreply.github.com>
2020-01-19 19:28:18 +0800

    Update 8_vcontact2.sh

--- 8_vcontact2.sh
+++ 8_vcontact2.sh
@@ -1,8 +1,11 @@
-#less prodigal_dereplicated_total_human_gut_virome_database.faa|sed -n "/>/p"|cut -d " " -f 1 >> name.txt
-#less prodigal_dereplicated_total_human_gut_virome_database.faa|sed -n "/>/p"|cut -d "." -f 1 >> name_2.txt
-#paste -d , name.txt name_2.txt > g2g.txt
-#sed -i 's/$/,/g' g2g.txt
-#sed -i "s/$/not_provided/g" g2g.txt
+less prodigal_dereplicated_total_human_gut_virome_database.faa|sed -n "/>/p"|cut -d " " -f 1 >> name.txt
+less prodigal_dereplicated_total_human_gut_virome_database.faa|sed -n "/>/p"|cut -d "." -f 1 >> name_2.txt
+paste -d , name.txt name_2.txt > g2g.txt
+sed -i 's/$/,/g' g2g.txt
+sed -i "s/$/not_provided/g" g2g.txt
+touch title.txt
+echo protein_id,contig_id,keywords > title.txt
+cat title.txt g2g.txt > g2g_2.csv
 
 #vcontact --raw-proteins [proteins file] --rel-mode ‘Diamond’ --proteins-fp [gene-to-genome mapping file] --db 'ProkaryoticViralRefSeq94-Merged' --pcs-mode MCL --vcs-mode ClusterONE --c1-bin [path to ClusterONE] --output-dir [target output directory]
 source activate vContact2
