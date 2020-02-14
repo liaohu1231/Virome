ddc3b74a25671342a46b2a2fe12b2e4a67fa94ff
liaohu1231 <59506038+liaohu1231@users.noreply.github.com>
2020-02-15 03:03:08 +0800

    Update blastn_NT.sh

--- blastn_NT.sh
+++ blastn_NT.sh
@@ -1,1 +1,1 @@
-source activate roary; blastn -db /ldfssz1/ST_META/share/database/NCBI/blast_nt/nt -evalue 1e-10 -max_target_seqs 1 -query Hu_final.contig.res_3000.fa -num_threads 40 -out evalue.txt -outfmt "6 qseqid sseqid pident qcovs length evalue"
+source activate roary; blastn -db db_path -evalue 1e-10 -max_target_seqs 1 -query Hu_final.contig.res_3000.fa -num_threads 40 -out evalue.txt -outfmt "6 qseqid sseqid pident qcovs length evalue"
