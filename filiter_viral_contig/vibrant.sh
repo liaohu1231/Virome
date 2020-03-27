5aac0cdd85525e8cd3f95058c17fd0f71599e124
liaohu1231 <59506038+liaohu1231@users.noreply.github.com>
2020-03-27 15:34:51 +0800

    Update vibrant.sh

--- filiter_viral_contig/vibrant.sh
+++ filiter_viral_contig/vibrant.sh
@@ -1,7 +1,5 @@
 source activate VIBRANT
-program=$(which VIBRANT_run.py)
-workdir=$(cd $(dirname $0); pwd)
-for i in $(ls HuVirDB/*.fa);do 
+for i in $(ls HuVirDB/HuVirDB.fa);do 
 echo $i
-python $program -i $i -t 20
+python VIBRANT_run.py -i $i -t 20
 done
