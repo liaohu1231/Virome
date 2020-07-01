8ccf992877dac850387f148196bc574a35a96bb5
liaohu1231 <59506038+liaohu1231@users.noreply.github.com>
2020-07-01 23:10:31 +0800

    Update vibrant.sh

--- filter_viral_contig/vibrant.sh
+++ filter_viral_contig/vibrant.sh
@@ -1,5 +1,5 @@
 source activate VIBRANT
 for i in $(ls HuVirDB/HuVirDB.fa);do 
 echo $i
-python2 $(which VIBRANT_run.py) -i $i -t 20 -folder vibrant_out/
+python $(which VIBRANT_run.py) -i $i -t 20 -folder vibrant_out/
 done
