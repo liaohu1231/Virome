46c1d0217ec56d9926c17ba8826f1f52524842ae
liaohu1231 <59506038+liaohu1231@users.noreply.github.com>
2022-03-16 20:32:49 +0800

    Update contig_len_info.py

--- contig_len_info.py
+++ contig_len_info.py
@@ -24,9 +24,6 @@ for k,v in line.items():
 	line[k] = len(v)
 
 seq_len_sort = sorted(line.items(), key=lambda x: x[1]) 
-###sorted(d.items(), key=lambda x: x[1]) 中 d.items() 为待排序的对象；
-###key=lambda x: x[1] 为对前面的对象中的第二维数据（即value）的值进行排序。
-###key=lambda  变量：变量[维数] 。维数可以按照自己的需要进行设置。
 seq_len_sort = dict(seq_len_sort)
 
 for k,v in seq_len_sort.items():
